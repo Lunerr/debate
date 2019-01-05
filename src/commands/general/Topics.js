23cfe991ea72a58e7d589243f2c95a27b7821db1
Lunerr <kyleroe94@gmail.com>
2019-01-05 11:24:28 -0600

    slight bug fix

--- src/commands/general/Topics.js
+++ src/commands/general/Topics.js
@@ -18,7 +18,7 @@ class Topics extends patron.Command {
       return msg.createErrorReply("there's currently no active topics in this server.");
 
     for (let i = 0; i < topics.length; i++) {
-      message += `**${topics[i].index + 1}.** ${topics[i].topic}\n\n`;  
+      message += `**${topics[i].index}.** ${topics[i].topic}\n\n`;  
 
       if (i === 20) {
         await msg.author.tryDM(message, {title: "Debate Topics"});
