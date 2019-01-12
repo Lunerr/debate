1415fcb1f9b3b9c7a42eb222ef8c2403f4cd8ed2
John <vim2meta@tutanota.com>
2019-01-11 19:51:14 -0500

    double the reply wait

--- src/utility/Constants.js
+++ src/utility/Constants.js
@@ -70,7 +70,7 @@ You may use \`;commands\` to view all commands.`};
     this.statements = {
       maxOpponents: 3,
       previousMessagesCount: 20,
-      replyWait: 60000,
+      replyWait: 120000,
       lastMessageAge: 60000
     };
 
